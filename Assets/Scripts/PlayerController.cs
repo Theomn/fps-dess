b5a03251b10b9007259f8297014b78185080b7d3
Theo <theo_menard@hotmail.fr>
2022-11-11 19:08:13 -0500

    Test - changement de PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int a = 5;
     }
 
     // Update is called once per frame
