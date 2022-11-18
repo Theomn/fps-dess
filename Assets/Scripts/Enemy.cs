7668cd69472293aa408f1d94893ca815240a16f6
Chestrade <anton.znyy@gmail.com>
2022-11-18 12:06:51 -0500

    Update enemy script
    Ajout de calcul de distance entre joueur et enemi

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -11,30 +11,52 @@ public class Enemy : MonoBehaviour
     [SerializeField] private int enemyMaxHealth;
 
     [Header("Aim")]
-    [SerializeField] private Transform playerPosition;
+    [SerializeField] private Transform target;
     [SerializeField] private Transform bulletSpawn;
     [SerializeField] private GameObject EnemyBulletPrefab; 
     [SerializeField] private int enemyRotationSpeed;
 
+    private float distanceToPlayer; //calcul de la distance entre le turret et le joueur.
 
 
-    void update()
+
+    void Update()
     {
+        //calcul de la distance entre enemi et le joueur
+        distanceToPlayer = Vector3.Distance(this.gameObject.transform.position, target.position);
+        Debug.Log(distanceToPlayer);
+
+
         //Gestion de health
-        if(enemyHealth >= enemyMaxHealth)
+        if (enemyHealth >= enemyMaxHealth)
         {
             enemyHealth = enemyMaxHealth;   
         }
         if(enemyHealth <= 0)
         {
-            die();
+            Die();
         }
-
-
+       
     }
 
+    void FixedUpdate()
+    {
+       
+        TargetPlayer();
+    }
 
-    void die()
+    void TargetPlayer()
+    {
+        /*
+
+        if(target == null) //ajouter || player.health <= 0
+        {
+            target = 
+        }
+        */
+                
+    }
+    void Die()
     {
         Destroy(gameObject);
     }
