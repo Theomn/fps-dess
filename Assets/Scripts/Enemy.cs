d9095b1ea173cb15445da70e72276bece3bd4130
Theo <theo_menard@hotmail.fr>
2022-11-21 14:54:15 -0500

    Enemy fixes

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -11,25 +11,28 @@ public class Enemy : MonoBehaviour
     [SerializeField] private int enemyMaxHealth;
 
     [Header("Aim")]
-    [SerializeField] private Transform player;
     [SerializeField] private Transform bulletSpawn;
     [SerializeField] private GameObject EnemyBulletPrefab; 
     [SerializeField] private float enemyRotationSpeed;
+    [SerializeField] private float detectionRange;
     
 
-    private float distanceToPlayer; //calcul de la distance entre le turret et le joueur.
+    private float distanceToPlayer;
+    private Transform player;
+
+
 
     private void Start()
     {
         enemyMaxHealth = 1000;
-       
+        // Retrieve the only instance of PlayerController in the scene automatically
+        player = PlayerController.Instance.transform;
     }
 
     void Update()
     {
         //calcul de la distance entre enemi et le joueur
-        distanceToPlayer = Vector3.Distance(this.gameObject.transform.position, player.position);
-        //Debug.Log(distanceToPlayer);
+        distanceToPlayer = Vector3.Distance(transform.position, player.position);
 
 
         //Gestion de health
@@ -42,24 +45,22 @@ public class Enemy : MonoBehaviour
             enemyHealth = 0;
             Die();
         }
-       
-    }
 
-    void FixedUpdate()
-    {
         EnemyDetection();
+
     }
 
     void EnemyDetection()
     {
-        if (distanceToPlayer <= 20)
+        if (distanceToPlayer <= detectionRange)
         {
             //transform.LookAt(player.position); Ca marche mais on peut pas controller la vitesse.
             var step = enemyRotationSpeed * Time.deltaTime;
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, player.rotation, step);
+            var targetRotation = Quaternion.LookRotation(player.position - transform.position);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, step);
         }
-                
     }
+
     void Die()
     {
         Destroy(gameObject);
