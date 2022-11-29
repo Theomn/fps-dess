d2e7e88d78b4cffa2db293614b2c062de4aee488
DabraSan <gnome_22@hotmail.com>
2022-11-29 16:47:02 -0500

    UI update

--- Assets/Scripts/HUDController.cs
+++ Assets/Scripts/HUDController.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class HUDController : SingletonMonoBehaviour<HUDController>
 {
+    public Slider healthBarSlider;
+    public Slider energyBarSlider;
+
 
     protected override void Awake()
     {
@@ -20,15 +25,27 @@ public class HUDController : SingletonMonoBehaviour<HUDController>
     // Update is called once per frame
     void Update()
     {
-        
+        /// touche T permet de mettre 10 de damage au player
+        if (Input.GetKeyDown(KeyCode.T))
+        { 
+            PlayerController.Instance.Damage(10);
+        }
     }
 
     /// <summary>
     /// Sets the health bar to the given number.
     /// </summary>
     /// <param name="health"></param>
+    public void SetMaxHealth(float health)
+    {
+        healthBarSlider.maxValue = health;
+        healthBarSlider.value = health;
+        
+    }
+
     public void SetHealth(float health)
     {
+        healthBarSlider.value = health;
 
     }
 
@@ -36,9 +53,17 @@ public class HUDController : SingletonMonoBehaviour<HUDController>
     /// Sets the weapon energy bar to the given number.
     /// </summary>
     /// <param name="energy"></param>
+
+    public void SetMaxEnergy(float energy)
+    {
+        energyBarSlider.maxValue = energy;
+        energyBarSlider.value = energy;
+
+    }
+
     public void SetEnergy(float energy)
     {
-
+        energyBarSlider.value = energy;
     }
 
     /// <summary>
