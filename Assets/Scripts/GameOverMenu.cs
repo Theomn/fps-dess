101688ce4ed6df294b914b52a61dbb10869cacde
DabraSan <gnome_22@hotmail.com>
2022-12-02 11:50:26 -0500

    ajout Game Over Menu

---
+++ Assets/Scripts/GameOverMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenu : MonoBehaviour
+{
+
+
+
+
+    public void RestartGame()
+    {
+
+    }
+
+    public void SettingsButton()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
